e71f0f7e8e0236df12317fb45f30a4456fd22c1b
RajVarun <grajvarun@gmail.com>
2025-12-24 20:37:04 +0530

    TLE: collatz_conjecture.py

---
+++ START218C/5.Collatz_Conjecture_(Easy).py
@@ -0,0 +1,30 @@
+# cook your dish here
+
+# TLE
+for _ in range(int(input())): # T inputs
+    n = int(input())
+    
+    vis = {n}    # seen numbers
+    q = [n]      # queue
+    boo = False
+    
+    while q:
+        curr=q.pop(0)
+        
+        if curr==1:
+            boo = True
+            break
+        xor_next = (3^curr) + 1
+        if xor_next not in vis:
+            vis.add(xor_next)
+            q.append(xor_next)
+            
+        if curr%2==0:
+            nh = curr//2
+            if nh not in vis:
+                vis.add(nh)
+                q.append(nh)
+    if boo:
+        print("Yes")
+    else:
+        print("No")
